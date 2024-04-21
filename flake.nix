7c519888926795f288aa7d85171de203a0b1c397
Alex Stanciu <ag.stanciu@gmail.com>
2024-04-21 16:58:46 +0300

    move to unstable

--- flake.nix
+++ flake.nix
@@ -1,10 +1,11 @@
 {
   description = "system config";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
